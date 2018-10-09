5647f1a23b749425c20c7d2aa7dffb3921b57ab0
MD MAMUNUR RASHID <netmamun@yahoo.com>
2018-10-09 01:38:40 -0400

    links works

--- server.rb
+++ server.rb
@@ -13,5 +13,12 @@ get '/portfolio' do
 end
 
 get '/about_me' do
+@skills = ['git', 'HTML', 'CSS', 'Ruby']
+@interests = ['cats', 'art', 'music', 'films', 'coffee']
   erb :new_view
+end
+
+get '/favourites' do
+   @fav_links =["www.amazon.ca","www.ebay.ca","www.kijiji.ca","www.facebook.com","www.mail.yahoo.com"]
+  erb :favourites
 end
